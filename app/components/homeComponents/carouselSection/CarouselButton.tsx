9fc98b4f707920d2207674d271c4c15404696cdf
roshdyjr <ahmedrush96@gmail.com>
2024-11-17 17:23:49 +0200

    Product Carousel Hover

--- app/components/homeComponents/carouselSection/CarouselButton.tsx
+++ app/components/homeComponents/carouselSection/CarouselButton.tsx
@@ -14,8 +14,8 @@ const CarouselButton = ({
   return (
     <button
       onClick={onClick}
-      className={`p-3 md:p-5 bg-white rounded-full border border-black text-lg md:text-2xl flex justify-center items-center ${
-        disabled ? "text-lightGray" : ""
+      className={`p-3 md:p-5 bg-white rounded-full border border-black text-lg md:text-2xl flex justify-center items-center transition-all duration-300 hover:bg-darkGray ${
+        disabled ? "text-lightGray cursor-not-allowed" : ""
       }`}
       disabled={disabled}
     >
